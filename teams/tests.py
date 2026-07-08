de7d0097a913a4993c86f161fbea0228f597d4f8
Rahim Rashid <rahimrashid06@gmail.com>
2026-07-08 15:08:45 -0400

    basic auth

--- teams/tests.py
+++ teams/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Team
 
-# Create your tests here.
+class TeamBuilderTests(TestCase):
+
+    # 1. Test teams home page loading
+    def test_teams_home_page_status(self):
+        response = self.client.get(reverse('teams_home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'teams_home.html')
+
+    # 2. Test creating a new team inserts default 6 empty slots into DB
+    def test_team_creation_initializes_empty_slots(self):
+        response = self.client.post(reverse('create_team'), {'team_name': 'My VGC Team'})
+        self.assertEqual(response.status_code, 302) # Redirects to detail page
+        
+        team = Team.objects.first()
+        self.assertIsNotNone(team)
+        self.assertEqual(team.name, 'My VGC Team')
+        self.assertEqual(len(team.slots), 6)
+        self.assertEqual(team.slots[0]['pokemon_id'], None)
